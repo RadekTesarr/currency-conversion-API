73b4c2647bb532a32674143d6ef2e60107d543cd
RadekTesař <radek.tesar135@gmail.com>
2024-03-24 21:19:17 +0100

    Scrollbar - currency

--- main.py
+++ main.py
@@ -16,5 +16,12 @@ user_input = Entry(width=20, font=("Arial", 12), justify=CENTER)
 user_input.insert(0, "0")
 user_input.grid(row=0, column=0, padx=10, pady=(10, 0))
 
+# Scrollbar - currency
+scrollbar_from = StringVar()
+scrollbar_from.set("CZK")   # Default value
+scrollbar_from_options = OptionMenu(root, scrollbar_from, "CZK", "EUR", "USD")
+scrollbar_from_options.grid(row=0, column=1, padx=10, pady=(10, 0))
+
+
 # Mainloop
 root.mainloop()
