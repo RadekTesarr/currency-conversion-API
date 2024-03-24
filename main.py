2579c1e25f3335ac2c482332cf33b8683ca19a0b
RadekTesař <radek.tesar135@gmail.com>
2024-03-24 21:14:18 +0100

    User input

--- main.py
+++ main.py
@@ -11,5 +11,10 @@ root.resizable(False, False)
 root.title("Currency conversion 2.0")
 root.config(bg=main_color)
 
+# User input
+user_input = Entry(width=20, font=("Arial", 12), justify=CENTER)
+user_input.insert(0, "0")
+user_input.grid(row=0, column=0, padx=10, pady=(10, 0))
+
 # Mainloop
 root.mainloop()
