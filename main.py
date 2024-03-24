f5e744a55a1a17e59e350a1e9bc6d1ab5d6faf1b
RadekTesař <radek.tesar135@gmail.com>
2024-03-24 21:09:50 +0100

    Created main window and added mainloop

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from tkinter import *
 import requests
+
+# Colors
+main_color = "#14085f"
+
+# Main Window
+root = Tk()
+root.minsize(400,120)
+root.resizable(False, False)
+root.title("Currency conversion 2.0")
+root.config(bg=main_color)
+
+# Mainloop
+root.mainloop()
