4050d457869bdb2ca85842a7bcc7cfd86d03c869
Radek <radek.tesar135@gmail.com>
2024-04-02 19:30:19 +0200

    Count function, API

--- main.py
+++ main.py
@@ -11,12 +11,35 @@ root.resizable(False, False)
 root.title("Currency conversion 2.0")
 root.config(bg=main_color)
 
+# Functions
+def count():
+    try:
+        currency_from = scrollbar_from.get()
+        currency_to = scrollbar_to.get()
+        amount = int(user_input.get())
+
+        # API
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={currency_to}&from={currency_from}&amount={amount}"
+
+        payload = {}
+        headers = {
+            "apikey": "RL9pMJ3r9by0ZVlW4JyrqBAKkZAxEvDo"
+        }
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        response.raise_for_status()
+        data_result = response.json()
+        result_label.config(text=round(data_result["result"], 2))
+    except:
+        notify_label.config(text="Zadejte částku")
+
 # User input
 user_input = Entry(width=20, font=("Arial", 12), justify=CENTER)
 user_input.insert(0, "0")
 user_input.grid(row=0, column=0, padx=10, pady=(10, 0))
 
-# Scrollbar - currency
+# Scrollbar - input currency
 scrollbar_from = StringVar(root)
 scrollbar_from.set("CZK")   # Default value
 scrollbar_from_options = OptionMenu(root, scrollbar_from, "CZK", "EUR", "USD")
@@ -29,7 +52,7 @@ scrollbar_to_options = OptionMenu(root, scrollbar_to, "EUR", "CZK", "USD")
 scrollbar_to_options.grid(row=1, column=1, padx=10, pady=(10, 0))
 
 # Convert button
-convert_button = Button(text="Přepočítat", font=("Arial", 12))
+convert_button = Button(text="Přepočítat", font=("Arial", 12), command=count)
 convert_button.grid(row=0, column=2, padx=10, pady=(10,0))
 
 # Result label
